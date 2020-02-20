376e65f900f097bca43713b2920b12d0c3de2cb5
Sean Wrigley <wrigles@gmail.com>
2020-02-20 13:24:47 +1100

    finally

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+
+  # root to: 'pages#home'
+
 end
